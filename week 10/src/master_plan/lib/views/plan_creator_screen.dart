9911792648b905efa23e887cafee2cbf798979a3
Amanda Vanika <amandavanika4@gmail.com>
2024-11-13 20:53:17 +0700

    Pertemuan 10

---
+++ week 10/src/master_plan/lib/views/plan_creator_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:master_plan/provider/plan_provider.dart';
+import 'package:master_plan/views/plan_screen.dart';
+
+import '../models/data_layer.dart';
+
+class PlanCreatorScreen extends StatefulWidget {
+  @override
+  State<PlanCreatorScreen> createState() => _PlanCreatorScreenState();
+}
+
+class _PlanCreatorScreenState extends State<PlanCreatorScreen> {
+  final textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ganti ‘Namaku' dengan nama panggilan Anda
+      appBar: AppBar(title: const Text('Master Plan Mandes')),
+      body: Column(children: [
+        _buildListCreator(),
+        Expanded(child: _buildMasterPlans())
+      ]),
+    );
+  }
+
+  Widget _buildListCreator() {
+    return Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Material(
+          color: Theme.of(context).cardColor,
+          elevation: 10,
+          child: TextField(
+              controller: textController,
+              decoration: const InputDecoration(
+                  labelText: 'Add a plan', contentPadding: EdgeInsets.all(20)),
+              onEditingComplete: addPlan),
+        ));
+  }
+
+  Widget _buildMasterPlans() {
+    ValueNotifier<List<Plan>> planNotifier = PlanProvider.of(context);
+    List<Plan> plans = planNotifier.value;
+
+    if (plans.isEmpty) {
+      return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Icon(Icons.task_rounded, size: 100, color: Colors.grey),
+            Text('You Have No Task To Do!',
+                style: Theme.of(context).textTheme.headlineSmall)
+          ]);
+    }
+    return ListView.builder(
+        itemCount: plans.length,
+        itemBuilder: (context, index) {
+          final plan = plans[index];
+          return ListTile(
+              title: Text(plan.name),
+              subtitle: Text(plan.completenessMessage),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (_) => PlanScreen(
+                          plan: plan,
+                        )));
+              });
+        });
+  }
+
+  void addPlan() {
+    final text = textController.text;
+    if (text.isEmpty) {
+      return;
+    }
+    final plan = Plan(name: text, tasks: []);
+    ValueNotifier<List<Plan>> planNotifier = PlanProvider.of(context);
+    planNotifier.value = List<Plan>.from(planNotifier.value)..add(plan);
+    textController.clear();
+    FocusScope.of(context).requestFocus(FocusNode());
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+}
